a841ef87e0b1afb041ee3bfa6c49ce8adcca4118
andreiacha <andreiacha@gmail.com>
2011-05-27 03:33:03 +0200

    RESTful metadata and item separeted!

--- webapp/whisperer/whisperer/restful.py
+++ webapp/whisperer/whisperer/restful.py
@@ -20,40 +20,54 @@ def add_user(request):
 	
 @view_config(name='addItem', context='whisperer.models.MyApp',
              renderer='json')
-def add_completeItem(request):
+def add_Item(request):
 	item_name = request.GET.get('item_name')
-	metadata_name = request.GET.get('metadata_name')
-	metadata_type = request.GET.get('metadata_type')
-	metadata_lang = request.GET.get('metadata_lang')
 	dataSet = request.GET.get('dataSet')
 	if not item_name:
 		return dict()
 	session = DBSession()
 	if not session.query(Item).filter(Item.itemName.in_([item_name])).all():			
-		metadata_added = Metadata(metadata_name, metadata_type, metadata_lang)
 		item_added = Item(item_name, dataSet)
 		session.add(item_added)
+		session.flush()
+		transaction.commit()
+		added_item_obj = session.query(Item).filter(Item.itemName.in_([item_name])).first()
+		return dict(added_item = str(added_item_obj.itemName), added_item_dataset = str(added_item_obj.dataSet))                			
+	return dict(message = 'Item already exists, please insert another')	
+
+@view_config(name='addinICM', context='whisperer.models.MyApp',
+             renderer='json')
+def add_Metadata_to_Item(request):
+	item_name = request.GET.get('item_name')
+	metadata_name = request.GET.get('metadata_name')
+	metadata_type = request.GET.get('metadata_type')
+	metadata_lang = request.GET.get('metadata_lang')
+	if not item_name:
+		return dict()
+	session = DBSession()
+	if not session.query(Item).filter(Item.itemName.in_([item_name])).all():
+		return dict(warning='No item name matched! Please, add the item on the DB first')				
+	if not session.query(Metadata).filter(Metadata.metaName.in_([metadata_name])).all():			
+		metadata_added = Metadata(metadata_name, metadata_type, metadata_lang)
 		session.add(metadata_added)
 		session.flush()
 		transaction.commit()
-		#get objects to use in ICM
-		added_item_obj = session.query(Item).filter(Item.itemName.in_([item_name])).first()
-		added_metadata_obj = session.query(Metadata).filter(Metadata.metaName.in_([metadata_name])).first()	
-		#Adds in ICM
-		ICMcell_added = ICMcell(added_item_obj.id, added_metadata_obj.id)
-		session.add(ICMcell_added)
-		session.flush()
-		transaction.commit()
-		#I must get everything again to use it!		
-		added_item_obj = session.query(Item).filter(Item.itemName.in_([item_name])).first()
-		added_metadata_obj = session.query(Metadata).filter(Metadata.metaName.in_([metadata_name])).first()	
-		added_cell_obj = session.query(ICMcell).filter(ICMcell.itemId.in_([added_item_obj.id])).first()	
-		#added_row = session.query(ICMcell).filter(ICMcell.id.in_([ICMcell_added.id])).all()
-		return dict(ICMcell_added_item = str(added_cell_obj.itemId), 
-		ICMcell_added_metadata = str(added_cell_obj.metadataId), added_item = str(added_item_obj.itemName), 
-		added_metadata = str(added_metadata_obj.metaName))                			
-	return dict(message = 'Item already exists, please insert another')	
-
+	#get objects to use in ICM
+	added_item_obj = session.query(Item).filter(Item.itemName.in_([item_name])).first()
+	added_metadata_obj = session.query(Metadata).filter(Metadata.metaName.in_([metadata_name])).first()	
+	#Adds in ICM
+	ICMcell_added = ICMcell(added_item_obj.id, added_metadata_obj.id)
+	session.add(ICMcell_added)
+	session.flush()
+	transaction.commit()
+	#Necessary to get again
+	added_item_obj = session.query(Item).filter(Item.itemName.in_([item_name])).first()
+	added_metadata_obj = session.query(Metadata).filter(Metadata.metaName.in_([metadata_name])).first()		
+	added_cells_list = session.query(ICMcell).filter(ICMcell.itemId.in_([added_item_obj.id])).all()
+	added_cell_obj = added_cells_list[-1]		
+	return dict(ICM_itemID = str(added_cell_obj.itemId), ICM_metadataID = str(added_cell_obj.metadataId),
+		added_metadata = str(added_metadata_obj.metaName))                			      			
+	
 
 @view_config(name='addRating', context='whisperer.models.MyApp',
              renderer='json')
