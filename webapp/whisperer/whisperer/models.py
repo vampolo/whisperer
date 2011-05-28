e93d97474a0e10d2db104b239677fc17396c79b2
Vincenzo Ampolo <vincenzo.ampolo@gmail.com>
2011-05-28 22:29:14 +0200

    ICM and URM now can be created!

--- webapp/whisperer/whisperer/models.py
+++ webapp/whisperer/whisperer/models.py
@@ -9,7 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import NoResultFound
 
-from sqlalchemy import Integer, String, Unicode, DateTime, ForeignKey, Column, create_engine, Table
+from sqlalchemy import Integer, String, Unicode, DateTime, ForeignKey, Column, Table
 
 from zope.sqlalchemy import ZopeTransactionExtension
 
@@ -19,29 +19,29 @@ Base = declarative_base()
 class Rating(Base):
 	__tablename__ = 'rating'
 	id = Column(Integer, primary_key=True)
-	userId = Column(Integer, ForeignKey('user.id'))
-	itemId = Column(Integer, ForeignKey('item.id'))
+	user_id = Column(Integer, ForeignKey('user.id'))
+	item_id = Column(Integer, ForeignKey('item.id'))
 	rating = Column(Integer)
 	timestamp = Column(DateTime)
 	
 	#many ratings refer to one item (many-to-one)
-	item= relationship("Item", backref="ratings")
+	item = relationship("Item", backref="ratings")
 
-	def __init__(self, userId, itemId, rating):
-		self.userId = userId
-		self.itemId = itemId
+	def __init__(self, user, item, rating):
+		self.user = user
+		self.item = item
 		self.rating = rating
 
 class User(Base):
 	__tablename__ = 'user'
 	id = Column(Integer, primary_key=True)
-	username = Column(Unicode(255), unique=True)
+	name = Column(Unicode(255), unique=True)
 
 	#one user has many ratings (one to many)
 	ratings = relationship("Rating", backref="user")
 
 	def __init__(self, username):
-		self.username = username
+		self.name = username
 
 items_metadatas_table = Table('items_metadatas', Base.metadata,
     Column('item_id', Integer, ForeignKey('item.id')),
@@ -51,29 +51,31 @@ items_metadatas_table = Table('items_metadatas', Base.metadata,
 class Item(Base):
 	__tablename__ = 'item'
 	id = Column(Integer, primary_key=True)
-	itemName = Column(Unicode(255))
-	dataSet = Column(Unicode(255))
+	name = Column(Unicode(255))
+	dataset = Column(Unicode(255))
 	
 	#many metadatas refer to many items (many-to-one)
 	metadatas = relationship("Metadata", 
 							 secondary=items_metadatas_table,
 							 backref="items")
 	
-	def __init__(self, itemName, dataSet):
-		self.itemName = itemName
-		self.dataSet = dataSet
+	def __init__(self, name, dataset=None):
+		self.name = name
+		self.dataset = dataset
 
 class Metadata(Base):
 	__tablename__ = 'metadata'
 	id = Column(Integer, primary_key=True)
-	metaName = Column(Unicode(255))
-	metaType = Column(Unicode(255))
-	metaLang = Column(Unicode(255))
-
-	def __init__(self, metaName, metaType, metaLang):
-		self.metaName = metaName
-		self.metaType = metaType
-		self.metaLang = metaLang
+	name = Column(Unicode(255))
+	type = Column(Unicode(255))
+	lang = Column(Unicode(255))
+	
+	#metadata-item relation set on item
+	
+	def __init__(self, name, type, lang):
+		self.name = name
+		self.type = type
+		self.lang = lang
 
 class MyModel(Base):
     __tablename__ = 'models'
@@ -124,6 +126,82 @@ root = MyApp()
 def default_get_root(request):
     return root
 
+def populate_database():
+	'''
+	populate database with fake data
+	'''
+	from random import randint
+	
+	session = DBSession()
+	
+	#create some users for first
+	users = ['Andeia', 'Vincenzo', 'Leonardo', 'Paolo']
+	items = ['film1', 'film2', 'film3', 'film4', 'film5']
+	metadatas = [dict(name='primo metadato', type='boh', lang='eng'),
+				 dict(name='secondo metadato', type='boh2', lang='eng'),
+				 dict(name='metadato3', type='boh3', lang='eng'),
+				 dict(name='metadato4', type='boh4', lang='eng')]
+	
+	
+	for meta in metadatas:
+		if not session.query(Metadata).filter(Metadata.name.in_([meta.get('name')])).all():
+			metadata = Metadata(meta.get('name'), meta.get('type'), meta.get('lang'))
+			session.add(metadata)
+			session.flush()
+	
+	metadatas = session.query(Metadata).all()
+	max_metadata = len(metadatas)-1
+
+	for item in items:
+		if not session.query(Item).filter(Item.name.in_([item])).all():			
+			new_item = Item(item)
+			new_item.metadatas.append(metadatas[randint(0,max_metadata)])
+			session.add(new_item)
+			session.flush()
+
+	items = session.query(Item).all()
+	max_item = len(items)-1
+	
+	for username in users:
+		if not session.query(User).filter(User.name.in_([username])).all():
+			user = User(username)
+			user.ratings.append(Rating(user, items[randint(0, max_item)], randint(0,5)))
+			session.add(user)
+			session.flush()
+	
+	users = session.query(User).all()
+	max_user = len(users)-1
+	
+	ratings = session.query(Rating).all()
+	max_ratings = len(ratings)
+	
+	#construct the URM now
+	from numpy import *
+	URM = zeros((len(users),len(items)))
+	for r in ratings:
+		URM[r.user_id-1][r.item_id-1] = r.rating
+	print URM
+	print 'Verify'
+	print [(r.user_id, r.item_id, r.rating) for r in ratings]
+	
+	
+	#construct the ICM now
+	ICM = zeros((len(metadatas),len(items)))
+	for i in items:
+		for m in i.metadatas:
+			ICM[m.id-1][i.id-1] = 1
+	print ICM
+	print 'Verify'
+	print [(metadata.id, item.id, 1) for item in items for meta in item.metadatas]
+	
+	from pymatlab.matlab import MatlabSession
+	session = MatlabSession('matlab -nodisplay')
+	session.putvalue('URM', URM)
+	session.run('B=sparse(URM)')
+	session.run('A=full(B)')
+	print session.getvalue('A')
+	
+	
 def initialize_sql(engine):
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
@@ -131,17 +209,12 @@ def initialize_sql(engine):
     try:
 		session = DBSession()
 		model = MyModel(name=u'test name', value=55)
-		#user1 = user(username='John')
-    		#item1 = item(itemName='Inglourious Bastards', dataSet='NetFlix')	
-	    	#metadata1 = metadata(metaName='Brad Pitt', metaType='actor', metaLang='eng')
-		#urmTest = URMcell(userId=user1.id, itemId=item1.id, rating=5)
-    		#icmTest = ICMcell(itemId=item1.id, metadataId=metadata1.id)
-		#session.add_all(user1, item1, metadata1, urmTest, icmTest)    	    	
 		session.add(model)	
 		session.flush()
 		transaction.commit()
     except IntegrityError:
         DBSession.rollback()
+    #populate_database()    	    	
     
 	
 
