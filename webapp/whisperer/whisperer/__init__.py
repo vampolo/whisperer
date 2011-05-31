5dd0255c0323ae5be99041eb5b8429511c65317e
andreiacha <andreiacha@gmail.com>
2011-05-31 03:21:53 +0200

    Views to create models added

--- webapp/whisperer/whisperer/__init__.py
+++ webapp/whisperer/whisperer/__init__.py
@@ -1,5 +1,8 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
+from pyramid.events import subscriber
+from pyramid.events import BeforeRender
+from pyramid.renderers import get_renderer
 
 from models import appmaker
 
@@ -9,10 +12,15 @@ def main(global_config, **settings):
     	engine = engine_from_config(settings, 'sqlalchemy.')
     	get_root = appmaker(engine)
     	config = Configurator(settings=settings, root_factory=get_root)
+    	config.add_subscriber(add_renderer_globals, BeforeRender)
     	config.add_static_view('static', 'whisperer:static')
     	config.add_view('whisperer.views.view_root', context='whisperer.models.MyApp', renderer="templates/root.pt")
         config.add_view('whisperer.views.view_model', context='whisperer.models.MyModel', renderer="templates/model.pt")
     	config.scan()
-   	return config.make_wsgi_app()
+	return config.make_wsgi_app()
+
+def add_renderer_globals(event):
+	event.update({'base': get_renderer('templates/root.pt').implementation()})
+	
 
 
--- webapp/whisperer/whisperer/views.py
+++ webapp/whisperer/whisperer/views.py
@@ -1,7 +1,15 @@
+from pyramid.view import view_config
+#get all classes from the models to be used
+from models import *
+
+@view_config(name='single_model', context='whisperer.models.MyApp',
+             renderer='templates/model.pt')
+def admin(request):
+	return dict()
+
 def view_root(context, request):
     return {'items':list(context), 'project':'whisperer'}
 
 def view_model(context, request):
     return {'item':context, 'project':'whisperer'}
-
-
+    
