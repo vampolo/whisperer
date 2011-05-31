60e2202d6e60a01b9b99b6f6811e045d26a8b953
Vincenzo Ampolo <vincenzo.ampolo@gmail.com>
2011-05-31 02:05:37 +0200

    first recommendation got

--- webapp/whisperer/whisperer/whisperer.py
+++ webapp/whisperer/whisperer/whisperer.py
@@ -4,7 +4,7 @@ from models import User, Item, Rating, Metadata, DBSession
 import os
 import functools
 
-ALGOPATH='../../algoritms'
+ALGOPATH='../../algorithms'
 
 def clean(f):
 	@functools.wraps(f)
@@ -20,13 +20,17 @@ class Whisperer(object):
 		self.m = MatlabSession('matlab -nosplash -nodisplay')
 		print "addpath(genpath('"+os.path.abspath(ALGOPATH)+"'))"
 		self.m.run("addpath(genpath('"+os.path.abspath(ALGOPATH)+"'))")
+		self.savepath = os.path.join(os.path.abspath(ALGOPATH), 'saved')
 		self.db = DBSession()
 	
 	def _put(self, name, value):
-		self.m.put(name, value)
+		self.m.putvalue(name, value)
 	
+	def _run(self, command):
+		self.m.run(command)
+		
 	def _get(self, name):
-		return self.m.get(name)
+		return self.m.getvalue(name)
 	
 	def _clean(self):
 		self.m.run("clear")
@@ -57,10 +61,38 @@ class Whisperer(object):
 			for m in i.metadatas:
 				icm[m.id-1][i.id-1] = 1
 		return icm
+		
+	def create_userprofile(self, user, items=None):
+		if not items:
+			items = self.db.query(Item).all()
+		
+		ratings = self.db.query(Rating).all()
+		up = numpy.zeros((1,len(items)))
+		for r in ratings:
+			up[0][r.item.id-1] = r.rating
+		return up
 	
-	def create_model(self, algname):
-		pass
+	@clean
+	def create_model(self, algname, urm=None):
+		#function [model] = createModel_AsySVD(URM,param)
+		if not urm:
+			urm = self.create_urm()
+		self._put('urm', urm)
+		self._run("["+algname+"_model] = createModel_"+algname+"(urm)")
+		self._run("save('"+os.path.join(self.savepath, algname+'_model')+"', '"+algname+"_model')")
+			
 	
+	@clean
+	def get_rec(self, algname, user, **param):
+		#function [recomList] = onLineRecom_AsySVD (userProfile, model,param)
+		up = self.create_userprofile(user)
+		self._put('up', up)
+		self._run("param = struct()")
+		for k,v in param.iteritems():
+			self._run("param."+str(k)+" = "+str(v))
+		self._run("load('"+os.path.join(self.savepath, algname+'_model')+"', '"+algname+"_model')")
+		self._run("[rec] = onLineRecom_"+algname+"(up, "+algname+"_model, param)")
+		return self._get("rec")
 	
 	@clean	
 	def do_something(self):
@@ -69,5 +101,9 @@ class Whisperer(object):
 		print self.create_urm()
 		print 'ICM'
 		print self.create_icm()
+		print 'run AsySVD'
+		print self.create_model('AsySVD')
+		print 'get rec'
+		print self.get_rec('AsySVD', self.db.query(User).filter(User.id==2).first(), userToTest=2)
 		#do something
 		
